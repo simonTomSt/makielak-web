a89f7bb54e885ddfd7c0b782acc806fd5ae98dca
simonTomSt <simontomst.github@gmail.com>
2023-07-05 18:27:08 +0200

    Setup proj

--- next.config.js
+++ next.config.js
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/onas',
+        destination: '/about',
+        locale: false, // Use `locale: false` so that the prefix matches the desired locale correctly
+      },
+      {
+        source: '/oferta',
+        destination: '/offer',
+        locale: false,
+      },
+      {
+        source: '/uslugi',
+        destination: '/services',
+        locale: false,
+      },
+      {
+        source: '/certyfikaty',
+        destination: '/certificates',
+        locale: false,
+      },
+      {
+        source: '/kontakt',
+        destination: '/contact',
+        locale: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/app/contact/page.js
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <main>O nas</main>;
+}
