8fee2e5cdcf6060702807eed60e974377dab33fa
simonTomSt <simontomst.github@gmail.com>
2023-08-23 18:57:13 +0200

    Add translations file

---
+++ src/translations/pl.ts
@@ -0,0 +1,71 @@
+export const pl = {
+  routes: {
+    about_us: 'o nas',
+    offer: 'oferta',
+    services: 'usługi',
+    certificates: 'certyfikaty',
+    contact: 'kontakt',
+    dashboard: {
+      edit_offer: 'Edycja oferty',
+      products: 'Produkty',
+      categories: 'Kategorie produktów',
+      pages_edit: 'Edycja stron',
+      edit_home: 'Strona główna',
+      edit_contact: 'Kontakt',
+    },
+  },
+  home_page: {
+    welcome_section_btn: 'Dowiedz się więcej',
+    offer_section_title: 'Nasza oferta',
+    offer_section_btn: 'Zobacz całą oferte',
+  },
+  footer: {
+    title: 'Wydobywanie żwiru i piasku Mirosław Makielak',
+  },
+  auth: {
+    login: {
+      title: 'Zaloguj się do panelu',
+      email: 'Email',
+      email_placeholder: 'Podaj email',
+      password_placeholder: 'Podaj hasło',
+      password: 'Hasło',
+      log_in: 'Zaloguj się',
+      server_error: 'Uups, coś poszło nie tak spróbuj ponownie później',
+      validation_error: 'Niepoprawne hasło lub login',
+    },
+  },
+  dashboard: {
+    dashboard_panel: 'Panel Admina',
+    log_out: 'Wyloguj się',
+    welcome: 'Witaj Adminie',
+    help: 'Pomoc',
+  },
+  admin: {
+    edit: {
+      save: 'Zapisz',
+      saved_successfully: 'Zapisano zmianę',
+      could_not_save: 'Nie udało się zapisać, spróbuj ponownie później',
+      upload_image: 'Klinkij aby wybrać zdjęcie',
+      home: {
+        title: 'Edycja strony głównej',
+        welcome: {
+          title: 'Edytuj pierwszą sekcje strony górnej',
+        },
+        about_us: {
+          title: 'Edytuj sekcję o firmie',
+        },
+        services: {
+          title: 'Edytuj sekcję z usługami',
+          item_header: 'Edytuj usługę nr. {index}',
+          item_title: 'Tytuł usługi',
+          item_desc: 'Opis usługi',
+          item_illustration: 'Ilustracja usługi (obrazek obok tekstu):',
+        },
+      },
+    },
+  },
+  upload_file: {
+    selected_file: 'Wybrano plik:',
+    helper: 'Kliknij aby wybrać plik',
+  },
+};
